56663390bab0830223393a40f7fb93f02b79420c
mengld <68012656+mengld@users.noreply.github.com>
2020-08-06 12:48:27 +0800

    Create Solution2.java

---
+++ 0347-Top K Frequent Elements/Java/Solution2.java
@@ -0,0 +1,61 @@
+// https://leetcode-cn.com/problems/top-k-frequent-elements/submissions/
+// mengld
+// 2020-8-6
+// 使用Java封装的PriorityQueue
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+public class Solution {
+
+    private class Freq implements Comparable<Freq>{
+
+        public int e, freq;
+
+        public Freq(int e, int freq){
+            this.e = e;
+            this.freq = freq;
+        }
+
+        public int compareTo(Freq another){
+            if(this.freq < another.freq)
+                return -1;
+            else if(this.freq > another.freq)
+                return 1;
+            else
+                return 0;
+        }
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int num: nums){
+            if(map.containsKey(num))
+                map.put(num, map.get(num) + 1);
+            else
+                map.put(num, 1);
+        }
+
+        PriorityQueue<Freq> pq = new PriorityQueue<>();
+        for(int key: map.keySet()){
+            if(pq.size() < k)
+                pq.add(new Freq(key, map.get(key)));
+            else if(map.get(key) > pq.peek().freq){
+                pq.remove();
+                pq.add(new Freq(key, map.get(key)));
+            }
+        }
+
+        LinkedList<Integer> res = new LinkedList<>();
+        while(!pq.isEmpty())
+            res.add(pq.remove().e);
+        int[] result = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+}
