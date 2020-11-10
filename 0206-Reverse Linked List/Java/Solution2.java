b09b91f48ce42304e076d0b54a7441d92ee0f69e
Menng <68012656+mengld@users.noreply.github.com>
2020-11-10 09:32:21 +0800

    Create Solution2.java

---
+++ 0206-Reverse Linked List/Java/Solution2.java
@@ -0,0 +1,16 @@
+// 递归实现
+
+class Solution {
+
+    public ListNode reverseList(ListNode head) {
+
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode rev = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        
+        return rev;
+    }
+}
