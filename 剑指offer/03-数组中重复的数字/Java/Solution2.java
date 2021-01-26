44391182545683c5739b6fd02cded3747dd23d73
Menng <68012656+mengld@users.noreply.github.com>
2021-01-26 10:50:08 +0800

    Create Solution2.java

---
+++ 剑指offer/03-数组中重复的数字/Java/Solution2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findRepeatNumber(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != i) {
+                if (nums[i] == nums[nums[i]]) {
+                    return nums[i];
+                }
+
+                int temp = nums[i];
+                nums[i] = nums[temp];
+                nums[temp] = temp;
+            }
+        }
+
+        return -1;
+    }
+}
