ec1e2f0a42405ff3574ed8c8f3ce2ca2109a13cb
Menng <68012656+mengld@users.noreply.github.com>
2021-02-02 09:58:25 +0800

    Create Solution.java

---
+++ 剑指offer/06-从尾到头打印链表/Java/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] reversePrint(ListNode head) {
+
+        Stack<Integer> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+
+        int[] res = new int[stack.size()];
+        int size = stack.size();
+        for (int i = 0; i < size; i++) {
+            res[i] = stack.pop();
+        }
+
+        return res;
+    }
+}
