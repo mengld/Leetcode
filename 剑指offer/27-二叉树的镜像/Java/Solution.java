48a626a7c25b6afebcd525063bd8adeaa8a92481
Menng <68012656+mengld@users.noreply.github.com>
2021-02-04 18:36:05 +0800

    Create Solution.java

---
+++ 剑指offer/27-二叉树的镜像/Java/Solution.java
@@ -0,0 +1,19 @@
+class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode(int x) { val = x; }
+}
+class Solution {
+    public TreeNode mirrorTree(TreeNode root) {
+
+        if (root == null)
+            return null;
+
+        TreeNode temp = root.left;
+        root.left = mirrorTree(root.right);
+        root.right = mirrorTree(temp);
+
+        return root;
+    }
+}
