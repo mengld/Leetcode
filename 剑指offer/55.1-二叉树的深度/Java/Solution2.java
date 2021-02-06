cf94486a11021943e090f3c0661d2c5b530eb7e6
Menng <68012656+mengld@users.noreply.github.com>
2021-02-06 21:28:07 +0800

    Create Solution2.java

---
+++ 剑指offer/55.1-二叉树的深度/Java/Solution2.java
@@ -0,0 +1,14 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+
+        return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
