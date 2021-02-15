268efb64c9688126351a087bc70b8a326481bffa
Menng <68012656+mengld@users.noreply.github.com>
2021-02-15 15:11:44 +0800

    Create Solution2.java

---
+++ 剑指offer/48-最长不含重复字符的子字符串/Java/Solution2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int res = 0, tmp = 0;
+        for(int j = 0; j < s.length(); j++) {
+            int i = j - 1;
+            while(i >= 0 && s.charAt(i) != s.charAt(j)) i--; // 线性查找 i
+            tmp = tmp < j - i ? tmp + 1 : j - i; // dp[j - 1] -> dp[j]
+            res = Math.max(res, tmp); // max(dp[j - 1], dp[j])
+        }
+        return res;
+    }
+}
