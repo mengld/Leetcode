525d0de4ee2af5042c56572111d09ff0edef28d1
Menng <68012656+mengld@users.noreply.github.com>
2021-03-14 09:22:37 +0800

    Create Solution3.java

---
+++ 剑指offer/31-栈的压入、弹出序列/Java/Solution3.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+
+        Stack<Integer> stack = new Stack<>();
+        int i = 0, j = 0;
+        while (i < pushed.length) {
+            int in = pushed[i];
+            int out = popped[j];
+            if (in == out) {
+                j ++;
+                while (!stack.isEmpty() && stack.peek() == popped[j]){
+                    stack.pop();
+                    j ++;
+                }
+                i ++;
+            } else {
+                stack.push(in);
+                i ++;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        int[] pushed = {2, 1, 0};
+        int[] popped = {1, 2, 0};
+        Solution solution = new Solution();
+        System.out.println(solution.validateStackSequences(pushed, popped));
+    }
+}
